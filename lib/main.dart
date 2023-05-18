77091da363002d3dd91aa125c0fb4c31911f3758
Abeer7755 <abeeresyad@gmail.com>
2023-05-18 05:38:24 +0300

    add firebase for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:school_phone/screens/home/home_screen.dart';
 import 'package:school_phone/screens/rate/rate_screen.dart';
 import 'package:school_phone/screens/splash/splash_screen.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
